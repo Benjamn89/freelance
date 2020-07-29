480bd9e81ce7da6da9374bbe656ac37d0eef0356
unknown <newbennytal@gmail.com>
2020-07-29 23:54:16 +0300

    Adding new project

--- header.js
+++ header.js
@@ -166,7 +166,7 @@ const jobs = () => {
     document.querySelector(".jobs-p").classList.add("jobs-p-on");
     setTimeout(() => {
       document.querySelector(".jobs-my-jobs").classList.add("jobs-my-jobs-on");
-      for (var i = 0; i <= 1; i++) {
+      for (var i = 0; i <= 2; i++) {
         document
           .querySelectorAll(".jobs-to-load")
           [i].classList.add("jobs-to-load-on");
